9dee062d34618639e4dfd04c7db3460e89e7855d
ashish-coder <ashishanand0516@gmail.com>
2023-06-18 13:14:42 +0530

    exception lecture

---
+++ src/org/learn/exception/ExcDemo.java
@@ -0,0 +1,14 @@
+package org.learn.exception;
+
+public class ExcDemo {
+	
+	public static void main(String[] args) {
+		int a=10;
+	    int b=0;
+	    if(a==0 || b==0) {
+	    	//throw new ExcDemo
+	    }
+		
+	}
+
+}
